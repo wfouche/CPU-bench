0ce76cff198442ee5c627b3242793bff83529752
wfouche <werner.fouche@gmail.com>
2020-06-08 10:52:31 +0200

    Removed blank lines between includes.

--- src/compute_N.c
+++ src/compute_N.c
@@ -1,19 +1,12 @@
 /* ----------------------------------------------------------------------- */
 
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <sys/time.h>
-
 #include <time.h>
-
 #include <unistd.h>
-
 #include <pthread.h>
-
 #include <errno.h>
-
 #include <ctype.h>
 
 /* ----------------------------------------------------------------------- */
